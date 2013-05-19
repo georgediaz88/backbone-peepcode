d4d2aa380bc4f639a78ca44330d58706f4cbf198
George Diaz <georgediaz88@yahoo.com>
2013-05-18 22:47:23 -0500

    PUT Spec for Video

--- spec/controllers/video_controller_spec.rb
+++ spec/controllers/video_controller_spec.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 require 'factory_girl_rails'
 
 describe VideosController do
+
   describe "GET #index json" do
+    let!(:video) {FactoryGirl.create(:video)}
     it 'should assign all videos' do
-      video = FactoryGirl.create(:video)
       get :index, format: :json
       assigns(:videos).should eq([video])
     end
   end
 
   describe "GET #show json" do
+    let!(:video) {FactoryGirl.create(:video)}
     it 'should assign correct video to show' do
-      video = FactoryGirl.create(:video)
       get :show, id: video, format: :json
       assigns(:video).should eq(video)
     end
@@ -28,4 +29,16 @@ describe VideosController do
     end
   end
 
+  describe "PUT update json" do
+    let!(:video) {FactoryGirl.create(:video)}
+    it "should update attributes" do
+      video_p = FactoryGirl.attributes_for(:video, title: 'Star Wars')
+      expect{
+        put :update, id: video, video: video_p, format: :json
+        video.reload
+      }.to change(video, :title).from('Pump Up Jam1').to('Star Wars')
+    end
+  end
+
 end
+
